85c3867afd9b3b91360b4844ae327a4552abfcaa
Alexey-beercode <alsemkovbn@gmail.com>
2023-11-22 22:15:24 +0300

    Update controllers and queries and handlers

---
+++ DwellEase.Service/Queries/Creator/GetOperationsByPagesOwnerQuery.cs
@@ -0,0 +1,9 @@
+﻿using DwellEase.Domain.Entity;
+using MediatR;
+
+namespace DwellEase.Service.Queries.Creator;
+
+public class GetOperationsByPagesOwnerQuery : IRequest<List<ApartmentOperation>>
+{
+    public string Id { get; set; }
+}
