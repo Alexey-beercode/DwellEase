ed0dfe9f002884c911ac47ea7c97972f82064bae
Alexey-beercode <alsemkovbn@gmail.com>
2023-11-29 01:23:17 +0300

    Added methods in ApartmentPageController

--- DwellEase.WebAPI/Areas/Admin/Controllers/ApartmentPageController.cs
+++ DwellEase.WebAPI/Areas/Admin/Controllers/ApartmentPageController.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using System.Net;
+using DwellEase.Service.Services.Implementations;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,11 +14,41 @@ public class ApartmentPageController:ControllerBase
 {
     private readonly IMediator _mediator;
     private readonly ILogger<ApartmentPageController> _logger;
+    private readonly ApartmentPageService _apartmentPageService;
 
-    public ApartmentPageController(IMediator mediator, ILogger<ApartmentPageController> logger)
+    public ApartmentPageController(IMediator mediator, ILogger<ApartmentPageController> logger, ApartmentPageService apartmentPageService)
     {
         _mediator = mediator;
         _logger = logger;
+        _apartmentPageService = apartmentPageService;
     }
-    
+
+    [HttpGet("GetAllPages")]
+    public async Task<IActionResult> GetAllPages()
+    {
+        var response = await _apartmentPageService.GetAllAsync();
+        if (response.StatusCode!=HttpStatusCode.OK)
+        {
+            return BadRequest(response.Description);
+        }
+
+        return Ok(response.Data);
+    }
+
+    [HttpDelete("DeletePage")]
+    public async Task<IActionResult> DeleteApartmentPage([FromBody] string id)
+    {
+        if (Guid.TryParse(id,out var guid))
+        {
+            return BadRequest("Invalid id");
+        }
+        
+        var response = await _apartmentPageService.DeleteAsync(guid);
+        if (response.StatusCode!=HttpStatusCode.OK)
+        {
+            return BadRequest(response.Description);
+        }
+
+        return Ok();
+    }
 }
